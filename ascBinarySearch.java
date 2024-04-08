e07042b756976cfe73e8251f7cd91d8c27d17af9
prateekjha1111 <125044950+prateekjha1111@users.noreply.github.com>
2024-04-08 18:25:55 +0530

    Binary search for ascending order array.

--- ascBinarySearch.java
+++ ascBinarySearch.java
@@ -4,8 +4,9 @@ public class ascBinarySearch {
         // only for sorted arrays in asc order
         int start = 0;
         int end = arr.length - 1;
-        int mid = start + (end - start)/2;
         while (start <= end) {
+            int mid = start + (end - start)/2;
+            
             if(arr[mid] < target){
                 start = mid + 1;
             }
