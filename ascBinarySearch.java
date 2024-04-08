cb54714c9a96579f23579a2fb2fe2628ba236326
prateekjha1111 <125044950+prateekjha1111@users.noreply.github.com>
2024-04-08 12:05:16 +0530

    Binary search for an ascending order array.

---
+++ ascBinarySearch.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class ascBinarySearch {
+    public static int binarySearch(int[] arr, int target){
+        // only for sorted arrays in asc order
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+            
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid] < target){
+                start = mid + 1;
+            }
+            else {
+                end  = mid - 1;
+            }
+        }
+       return -1; // not found
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter elements: ");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter target: ");
+        int target = sc.nextInt();
+        int index = binarySearch(arr, target);
+
+        if (index != -1) {
+            System.out.print("Target found at index " + index);
+        } else {
+            System.out.print("Target not found!");
+        }
+        sc.close();
+    }
+}
